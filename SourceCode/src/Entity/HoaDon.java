07cede581c8a2877e059a38b238ec54de721e947
PhamHuuThuan <92664025+PhamHuuThuan@users.noreply.github.com>
2023-04-06 10:57:08 +0700

    add HoaDon.class

--- SourceCode/src/Entity/HoaDon.java
+++ SourceCode/src/Entity/HoaDon.java
@@ -1,8 +1,77 @@
 package Entity;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class HoaDon{
 	private String soHoaDon;
 	private Date ngayTaoHD;
+	ArrayList<ThanhVien> dsTV;
+	private TourDuLich tour;
+	private KhachHang kh;
+	private final float VAT = 0.1f;
+	
+	public double tinhThanhTien() {
+		return dsTV.size()*tour.getGia()*VAT;
+	}
+	public HoaDon(String soHoaDon, Date ngayTaoHD, ArrayList<ThanhVien> dsTV, TourDuLich tour, KhachHang kh) {
+		super();
+		this.soHoaDon = soHoaDon;
+		this.ngayTaoHD = ngayTaoHD;
+		this.dsTV = dsTV;
+		this.tour = tour;
+		this.kh = kh;
+	}
+	public HoaDon(String soHoaDon) {
+		this.soHoaDon = soHoaDon;
+	}
+	public HoaDon() {
+		
+	}
+	public String getSoHoaDon() {
+		return soHoaDon;
+	}
+	public void setSoHoaDon(String soHoaDon) {
+		this.soHoaDon = soHoaDon;
+	}
+	public Date getNgayTaoHD() {
+		return ngayTaoHD;
+	}
+	public void setNgayTaoHD(Date ngayTaoHD) {
+		this.ngayTaoHD = ngayTaoHD;
+	}
+	public ArrayList<ThanhVien> getDsTV() {
+		return dsTV;
+	}
+	public void setDsTV(ArrayList<ThanhVien> dsTV) {
+		this.dsTV = dsTV;
+	}
+	public TourDuLich getTour() {
+		return tour;
+	}
+	public void setTour(TourDuLich tour) {
+		this.tour = tour;
+	}
+	public KhachHang getKh() {
+		return kh;
+	}
+	public void setKh(KhachHang kh) {
+		this.kh = kh;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(soHoaDon);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HoaDon other = (HoaDon) obj;
+		return Objects.equals(soHoaDon, other.soHoaDon);
+	}
 }
